96a98a8df926e6649f87b782afa0a8c4a8f91e11
Nitram2704 <martinsanchez270804@gmail.com>
2025-10-31 17:12:21 -0600

    Co-authored-by: samuelmontoya1314 <samuelmontoya1314@users.noreply.github.com>

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,49 @@ app.get('/api/products', async (req, res) => {
   }
 });
 
-// Aquí añadiremos las rutas para interactuar con WooCommerce
+// Ruta para crear un nuevo pedido en WooCommerce
+app.post('/api/orders', async (req, res) => {
+  const { cart, customer } = req.body;
+
+  const orderData = {
+    payment_method: "bacs", // O el método de pago que prefieras
+    payment_method_title: "Direct Bank Transfer",
+    set_paid: true,
+    billing: {
+      first_name: customer.firstName,
+      last_name: customer.lastName,
+      email: customer.email,
+    },
+    shipping: {
+      first_name: customer.firstName,
+      last_name: customer.lastName,
+    },
+    line_items: cart.map(item => ({
+      product_id: item.id,
+      quantity: item.quantity,
+    })),
+    status: 'completed' // Marcar como completado
+  };
+
+  try {
+    const { data } = await WooCommerce.post('orders', orderData);
+    res.json(data);
+  } catch (error) {
+    console.error('Error al crear el pedido en WooCommerce:', error.response ? error.response.data : error.message);
+    res.status(500).json({ message: 'Error al crear el pedido', details: error.response ? error.response.data : null });
+  }
+});
+
+// Endpoint para obtener categorías
+app.get('/api/categories', async (req, res) => {
+  try {
+    const response = await WooCommerce.get('products/categories');
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error fetching categories:', error);
+    res.status(500).json({ error: 'Error fetching categories' });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
